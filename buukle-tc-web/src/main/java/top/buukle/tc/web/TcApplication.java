d0a59b111f7ad29a04e21e8b4f769fa204efd490
zhanglei1102 <zhanglei1102@jd.com>
2019-09-06 17:22:50 +0800

    inital commit

---
+++ buukle-tc-web/src/main/java/top/buukle/tc/web/TcApplication.java
@@ -0,0 +1,31 @@
+package top.buukle.tc.web;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.ApplicationContext;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@MapperScan("top.buukle.security.dao")
+@SpringBootApplication(scanBasePackages={"top.buukle.*"})
+@EnableFeignClients(basePackages = {"top.buukle.*"})
+@EnableRedisHttpSession
+public class TcApplication {
+    private static volatile boolean RUNNING = true;
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(TcApplication.class, args);
+        System.out.println("启动成功");
+        synchronized (TcApplication.class) {
+            while (RUNNING) {
+                try {
+                    TcApplication.class.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    SpringApplication.exit(context);
+                }
+            }
+        }
+    }
+}
